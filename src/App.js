9fb8cac9ecd3aa11811e697e2e28b5e658f068d1
wolfenmor <wolfenmor@gmail.com>
2024-02-03 18:01:52 +0100

    homework_2 in progress

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 import "./App.css"
-import Users from "./components/users/users"; // отдавать
-let users = [
-                {name: 'vasya', age: 31, status: false},
-                {name: 'petya', age: 30, status: true},
-                {name: 'kolya', age: 29, status: true},
-                {name: 'olya', age: 28, status: false},
-                {name: 'max', age: 30, status: true},
-                {name: 'anya', age: 31, status: false},
-                {name: 'oleg', age: 28, status: false},
-                {name: 'andrey', age: 29, status: true},
-                {name: 'masha', age: 30, status: true},
-                {name: 'olya', age: 31, status: false},
-                {name: 'max', age: 31, status: true}
-            ];
 function App() {
   return (
     <div>
-      <Users item={users}></Users>
+
     </div>
   );
 }
